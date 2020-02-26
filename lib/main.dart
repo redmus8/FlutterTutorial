b1fc0d800ca18fd1b26958bfb0d10df0b8f3659e
redmus8 <mustapha.sefsafi@etu.usthb.dz>
2020-02-26 13:44:35 +0100

    First flutter Mi-Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/original.jpg'),
+            ),
+            Text(
+              'Deku izuko',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Tomorrow'),
+            ),
+            Text(
+              'Hero Academia',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade200,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Tomorrow'),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 200.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+213 771 48 85 06',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Tomorrow',
+                      fontSize: 20,
+                    ),
+                  )),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'redmus8@hotmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Tomorrow',
+                      fontSize: 20,
+                    ),
+                  )),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
